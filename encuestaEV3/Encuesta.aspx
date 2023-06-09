5c4f4690e6fe3973d2db76414a1f2dfa37609f8c
kevinpasten <kevin.pasten02@inacapmail.cl>
2023-06-09 15:39:07 -0400

    dropdown

--- encuestaEV3/Encuesta.aspx
+++ encuestaEV3/Encuesta.aspx
@@ -1,4 +1,26 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Encuesta.aspx.cs" Inherits="encuestaEV3.WebForm1" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-    <h1>encuesta</h1>
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            <div CssClass="row">
+            <section CssClass="col-md-4">       
+            <asp:Label runat="server" Text="Nombre:" CssClass="form-label" AssociatedControlID="nombre"></asp:Label>
+            <asp:TextBox CssClass="form-control" ID="nombre" runat="server" MaxLength="40"></asp:TextBox>
+            <asp:Label CssClass="form-label p-2" runat="server" Text="Seleccionar rango de edad:" AssociatedControlID="DropDownList1"></asp:Label>
+                <asp:DropDownList ID="DropDownList1" runat="server">
+                <asp:ListItem Disabled="True" Enabled="True" Selected="True">--Seleccione--</asp:ListItem>
+                <asp:ListItem>18-34</asp:ListItem>
+                <asp:ListItem>35-60</asp:ListItem>
+                <asp:ListItem>61-100</asp:ListItem>
+            </asp:DropDownList>
+                <div CssClass="row">
+                    <asp:Label CssClass="form-label" runat="server" Text="Sexo:"></asp:Label>
+                    <asp:RadioButton ID="RadioButton1" runat="server" GroupName="genero" Text="Femenino"/>
+                    <asp:RadioButton ID="RadioButton2" runat="server" GroupName="genero" Text="Masculino"/>
+                    <asp:RadioButton ID="RadioButton3" runat="server" GroupName="genero" Text="Otro"/>
+                </div>
+        </section>
+    </div>                    
+        </ContentTemplate>
+    </asp:UpdatePanel>
 </asp:Content>
--- encuestaEV3/Encuesta.aspx.designer.cs
+++ encuestaEV3/Encuesta.aspx.designer.cs
@@ -13,5 +13,59 @@ namespace encuestaEV3
 
     public partial class WebForm1
     {
+
+        /// <summary>
+        /// Control UpdatePanel1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
+
+        /// <summary>
+        /// Control nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox nombre;
+
+        /// <summary>
+        /// Control DropDownList1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+
+        /// <summary>
+        /// Control RadioButton1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton RadioButton1;
+
+        /// <summary>
+        /// Control RadioButton2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton RadioButton2;
+
+        /// <summary>
+        /// Control RadioButton3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton RadioButton3;
     }
 }
